8c87f62252a7ee73c726b823e3d46100ef9844ce
venkat-rach <68603367+venkat-rach@users.noreply.github.com>
2021-11-04 18:07:23 +0530

    Create Javaprog1.java

---
+++ Javaprog1.java
@@ -0,0 +1,6 @@
+class Javaprog1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins"); 
+        System.out.println("Hello this is for jenkins java code test");
+    }
+}
