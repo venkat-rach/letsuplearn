6799a29ef6b06b08957a0bd2fe0a7cfb5d897942
venkat-rach <68603367+venkat-rach@users.noreply.github.com>
2021-11-04 18:45:30 +0530

    Javaprog

--- Javaprog.java
+++ Javaprog.java
@@ -1,4 +1,4 @@
-class Javaprog1 {
+class Javaprog {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins"); 
         System.out.println("Hello this is for jenkins java code test");
