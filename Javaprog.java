89dd24d9da540c77df5a8d48c9a1e2e21a1e2731
venkat-rach <68603367+venkat-rach@users.noreply.github.com>
2021-11-05 11:18:43 +0530

    Update Javaprog.java

--- Javaprog.java
+++ Javaprog.java
@@ -2,5 +2,6 @@ class Javaprog {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins"); 
         System.out.println("Hello this is for jenkins java code test");
+        System.out.println("This is for Jenkins and Git Poll SCM test");
     }
 }
