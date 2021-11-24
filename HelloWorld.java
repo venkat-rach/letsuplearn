1db7457f94615cd41e29ec938103a2b2a14ab819
venkat-rach <68603367+venkat-rach@users.noreply.github.com>
2021-11-24 17:14:03 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("Hello this is for jenkins Poll SCM test");
+        System.out.println("Hello this is for Jenkins Slave test");
     }
 }
