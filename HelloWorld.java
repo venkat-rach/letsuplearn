86617ac6682c0150c2c64bb3ebed01016c112d76
tester <tester@test.com>
2022-02-10 11:42:56 +0530

    Added Github local test

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("Hello this is for jenkins Poll SCM test");
-        System.out.println("Hello this is for Jenkins Slave test");
+        System.out.println("Hello this is for Github local test");
     }
 }
